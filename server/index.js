373392410205c1d7b6fa4a76eef07fb92f6ee983
hjha1110 <hjha1110@gmail.com>
2022-04-16 22:28:57 +0530

    some fixes

--- server/index.js
+++ server/index.js
@@ -11,8 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://hjha1110:Him%403129@cluster0.gx8yt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "";
 
 const PORT = process.env.PORT || 5000;
 
