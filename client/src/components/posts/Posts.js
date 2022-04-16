3e3e337ed3d1b1a057021fad0020b90dd8280d7e
hjha1110 <hjha1110@gmail.com>
2022-04-16 22:27:04 +0530

    added view cards for posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Grid, CircularProgress } from "@mui/material";
 import { useSelector } from "react-redux";
+
 import Post from "./post/Post";
 import { styles } from "./styles";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
-  console.log(posts);
-  return (
-    <>
-      <div>Posts</div>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid sx={styles.container} container alignItems="stretch" spacing={3}>
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
--- client/src/components/posts/post/Post.js
+++ client/src/components/posts/post/Post.js
@@ -1,10 +1,76 @@
-import React from 'react'
-import {styles} from './styles'
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+// import { ThumbUpAltIcon, DeleteIcon } from "@mui/icons-material";
+import moment from "moment";
+import { useDispatch } from "react-redux";
 
-const Post = () => {
+import { likePost, deletePost } from "../../../actions/posts";
+import { styles } from "./styles";
+
+const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
+
   return (
-    <div>Post</div>
-  )
-}
+    <Card sx={styles.card}>
+      <CardMedia
+        sx={styles.media}
+        image={
+          post.selectedFile ||
+          "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+        }
+        title={post.title}
+      />
+      <div sx={styles.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div sx={styles.overlay2}>
+        <Button
+          style={{ color: "white" }}
+          size="small"
+          onClick={() => setCurrentId(post._id)}
+        ></Button>
+      </div>
+      <div sx={styles.details}>
+        <Typography variant="body2" color="textSecondary" component="h2">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography sx={styles.title} gutterBottom variant="h5" component="h2">
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions sx={styles.cardActions}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
+          {/* <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}{" "}
+        </Button>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deletePost(post._id))}
+        >
+          <DeleteIcon fontSize="small" /> Delete */}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default Post
+export default Post;
